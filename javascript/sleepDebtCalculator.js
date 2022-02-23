fa94557e2f5f2e23b91115be2fb7b1e3f378551a
Ellie <dev.ellie7@gmail.com>
2022-02-23 22:55:41 +0900

    refactor

--- javascript/sleepDebtCalculator.js
+++ javascript/sleepDebtCalculator.js
@@ -1,52 +1,23 @@
-const getSleepHours = day => {
-  if (day === "Monday") {
-    return 8;
-  } else if (day === "Tuesday") {
-    return 7;
-  } else if (day === "Wednesday") {
-    return 8;
-  } else if (day === "Thursday") {
-    return 6;
-  } else if (day === "Friday") {
-    return 5;
-  } else if (day === "Saturday") {
-    return 5;
-  } else if (day === "Sunday") {
-    return 6;
-  }
-};
+const getActualSleepHours = () => 8 + 7 + 8 + 6 + 5 + 5 + 6;
 
-// console.log(getSleepHours('Wednesday'));
-
-const getActualSleepHours = () => {
-  return (
-    getSleepHours("Monday") +
-    getSleepHours("Tuesday") +
-    getSleepHours("Wednesday") +
-    getSleepHours("Thursday") +
-    getSleepHours("Friday") +
-    getSleepHours("Saturday") +
-    getSleepHours("Sunday")
-  );
-};
-
-const getIdealSleepHours = () => {
-  const idealHours = 7.5;
-  return idealHours * 7;
-};
+const getIdealSleepHours = idealHours => idealHours * 7;
 
 // console.log(getActualSleepHours());
-// console.log(getIdealSleepHours());
+// console.log(getIdealSleepHours(7.8));
 
 const calculateSleepDebt = () => {
   //실제 수면 시간과 이상적인 수면 시간을 알 수 있으므로 수면 부채를 계산할 차례
   const actualSleepHours = getActualSleepHours();
-  const idealSleepHours = getIdealSleepHours();
+  const idealSleepHours = getIdealSleepHours(7.5);
 
   if (actualSleepHours === idealSleepHours) {
     console.log("수면시간이 적절합니다.");
   } else if (actualSleepHours > idealSleepHours) {
-    console.log("필요 이상으로 수면을 취했습니다.");
+    console.log(
+      "필요 이상 " +
+        (actualSleepHours - idealSleepHours) +
+        "시간을 더 많이 잤네요. 수면 시간을 줄이세요."
+    );
   } else {
     console.log(
       idealSleepHours - actualSleepHours + "시간을 더 주무셔야합니다."
