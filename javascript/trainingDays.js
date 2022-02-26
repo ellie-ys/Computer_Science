90581fbb1588807ba544f1ba51e313fca7f4b721
Ellie <dev.ellie7@gmail.com>
2022-02-26 20:53:00 +0900

    random variable put into the getRandEvent function

--- javascript/trainingDays.js
+++ javascript/trainingDays.js
@@ -1,7 +1,7 @@
 // The scope of `random` is too loose
-const random = Math.floor(Math.random() * 3);
 
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
   if (random === 0) {
     return "Marathon";
   } else if (random === 1) {
